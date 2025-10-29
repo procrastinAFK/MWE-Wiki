33903f144b24e1aa0ea5f55eacbd124035e2be8c
Maya Berchin <mayab97@nycstudents.net>
2025-10-28 22:35:47 -0400

    Got logging in to work minimally... actual auth needs to be added alongside so much else

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,22 @@
 # p00
 # Softdev
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def login():
     return render_template('login.html');
+
+@app.route("/home", methods=['GET','POST']) # will need to use post for password at the very least
+def authenticate():
+    if (request.method == "POST"):
+        username = request.form.get('username','')
+    else:
+        username = request.args.get('username','')
+    return render_template('home.html', username=username) 
+
 
 if (__name__ == "__main__"):
     app.debug = True; # temporary!
