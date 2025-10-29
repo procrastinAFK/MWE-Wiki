9302e896d8933917b9e8dbc28cf9c1d2bac3becc
Evan Khosh <evank43@nycstudents.net>
2025-10-29 11:36:10 -0400

    Made beta of /register route

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,26 @@ def authenticate():
         username = request.args.get('username','')
     return render_template('home.html', username=username) 
 
+@app.route("/register", methods['GET', 'POST'])
+def register():
+    DB_FILE="data.db"
+
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    
+    c.execute("CREATE TABLE IF NOT EXISTS userdata(username TEXT, password TEXT, sign_up_date TEXT, bio TEXT, blog_ids TEXT, PRIMARY KEY(username))")
+    
+    if request.method == 'POST':
+        usernamedata c.execute("SELECT username FROM userdata")
+        for row in usernamedata:
+            if row[0] == request.form['username']:
+                #Throw error for same username
+                
+        #Needs value for sign_up_date
+        c.execute(f"INSTERT INTO userdata VALUES (\"{request.form['username']}\", \"{request.form['password']}\", \"\", \"\", \"\")")
+        return redirect("/")
+
+    return render_template('register.html')
 
 if (__name__ == "__main__"):
     app.debug = True; # temporary!
